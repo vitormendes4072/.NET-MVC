73fe5231bd25c00e0a8fc652769cb46b8bd38344
vitormendes4072 <vitormendes4072@gmail.com>
2021-08-23 11:49:39 -0300

    Utilização do ViewBag e ViewData, metódo Post e Get

--- Fiap.Web.Aula02.Exemplo01/Controllers/ClienteController.cs
+++ Fiap.Web.Aula02.Exemplo01/Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiap.Web.Aula02.Exemplo01.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,9 +18,23 @@ namespace Fiap.Web.Aula02.Exemplo01.Controllers
         //URL: /Cliente/Cadastrar
         //Criar uma Action Cadastrar que retorna uma página
         //A página terá um formulario HTML com os campos nome, cpf e botao
+        
+        [HttpGet]
         public IActionResult Cadastrar()
         {
             return View();
+        }
+
+        //Action que "cadastra no banco de dados"
+        [HttpPost]
+        public IActionResult Cadastrar(Cliente cliente)
+        {
+            ViewData["msg"] = "Cliente cadastrado";
+            ViewData["nome"] = cliente.Nome; //Envia o nome do cliente para a View
+            ViewBag.cpf = cliente.Cpf;
+            ViewBag.cliente = cliente;
+            return View();
+            //return Content($"Nome: {cliente.Nome}, CPF: {cliente.Cpf}");//Retorna um texto no browser
         }
     }
 }
