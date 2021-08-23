0ee90d69b186c16086f5857ca9da4b487b9767f9
vitormendes4072 <vitormendes4072@gmail.com>
2021-08-23 10:41:44 -0300

    Adicionar arquivos de projeto.

---
+++ Fiap.Web.Aula02.Exemplo01/Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.Aula02.Exemplo01.Controllers
+{
+    public class ClienteController : Controller
+    {
+        //URL: /Cliente/Index ou /Cliente
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //URL: /Cliente/Cadastrar
+        //Criar uma Action Cadastrar que retorna uma página
+        //A página terá um formulario HTML com os campos nome, cpf e botao
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
