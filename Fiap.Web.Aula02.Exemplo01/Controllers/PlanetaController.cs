0b3e9d7280538dc195a902b844ad1b6383df7b6f
vitormendes4072 <vitormendes4072@gmail.com>
2021-08-28 00:11:46 -0300

    Cadastro de planetas

---
+++ Fiap.Web.Aula02.Exemplo01/Controllers/PlanetaController.cs
@@ -0,0 +1,33 @@
+﻿using Fiap.Web.Aula02.Exemplo01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.Aula02.Exemplo01.Controllers
+{
+    public class PlanetaController : Controller
+    {
+        static IList<Planeta> _banco = new List<Planeta>();
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Planeta planeta)
+        {
+            _banco.Add(planeta);
+            ViewBag.banco = _banco;
+            return RedirectToAction("Cadastrar");
+        }
+
+        public IActionResult Listar()
+        {
+            ViewBag.banco = _banco;
+            return View();
+        }
+    }
+}
---
+++ Fiap.Web.Aula02.Exemplo01/Models/Planeta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.Aula02.Exemplo01.Models
+{
+    /**
+     * Criar uma página para cadastro -> Utilizando tag-Helpers
+     * Controller -> private static IList<Planeta> _banco;
+     * Criar uma página listar os planetas!
+     */
+    public class Planeta
+    {
+        public string Nome { get; set; }
+        public string DataDescoberta { get; set; }
+        public bool Habitavel { get; set; }
+        public int TempoRotacao { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nome}\r\n Data de descoberta: {DataDescoberta}\r\n Habitavel: {Habitavel}";
+        }
+    }
+}
